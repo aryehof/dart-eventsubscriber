53e708feb63aeaf8a03b01755f9b249eafffe375
Aryeh <aryeh@users.noreply.github.com>
2020-01-10 14:39:59 +0200

    Initial commit

---
+++ example/eventnotifier_flutter_example.dart
@@ -0,0 +1,6 @@
+import 'package:eventnotifier_flutter/eventnotifier_flutter.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/eventnotifier_flutter.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library eventnotifier_flutter;
+
+export 'src/eventnotifier_flutter_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
